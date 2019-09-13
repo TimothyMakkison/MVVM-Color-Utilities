2120929790c029885f49fd732059c365b4310637
TimothyMakkison <49349513+TimothyMakkison@users.noreply.github.com>
2019-09-13 19:47:10 +0100

    Fixed memory leak between IQ and IA
    Changed ImageBufferItems ColorCountList and QuantizerList get to return a unique list each time.  Therefore, using the quantizer in one instance no longer changed the other instances quantizer.

--- MVVM Color Utilities/Helpers/ImageBufferItems.cs
+++ MVVM Color Utilities/Helpers/ImageBufferItems.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using MVVM_Color_Utilities.Palette_Quantizers;
 using MVVM_Color_Utilities.Palette_Quantizers.Median_Cut;
 using Microsoft.Win32;
+using System.Linq;
 
 namespace MVVM_Color_Utilities.Helpers
 {
@@ -11,19 +12,38 @@ namespace MVVM_Color_Utilities.Helpers
     /// </summary>
     public static class ImageBufferItems
     {
-        public static  readonly OpenFileDialog OpenDialogBox = new OpenFileDialog()
+        #region Fields
+        private readonly static List<BaseColorQuantizer> _quantizerList = new List<BaseColorQuantizer>
+        {
+            new MedianCutQuantizer(),
+        };
+        private readonly static List<Int32> _colorCountList =
+           new List<int> { 1, 2, 4, 8, 16, 32, 64, 128, 256 };//{ 256,128,64,32,16,8,4,2,1};
+        #endregion
+
+        #region Properties
+        public static readonly OpenFileDialog OpenDialogBox = new OpenFileDialog()
         { Filter = "Images| *.jpg;*.png;*.jpeg;*.bmp", Title = "Browse Images" };
 
         public static readonly SaveFileDialog SaveDialogBox = new SaveFileDialog()
         { Filter = "JPG (*.jpg;*.jpeg)|(*.jpg;*.jpeg)", Title = "Save Image" };
         //|PNG(*.png)| (*.png)|BMP(*.bmp)| (*.bmp)"
 
-        public static List<Int32> ColorCountList { get; } =
-            new List<int> { 1, 2, 4, 8, 16, 32, 64, 128, 256 };//{ 256,128,64,32,16,8,4,2,1};
-
-        public static List<BaseColorQuantizer> QuantizerList { get; } = new List<BaseColorQuantizer>
+        public static List<Int32> ColorCountList
         {
-            new MedianCutQuantizer()
-        };
+            get
+            {
+                return _colorCountList.ToList();
+            }
+        }
+      
+        public static List<BaseColorQuantizer> QuantizerList
+        {
+            get
+            {
+                return _quantizerList.ToList();
+            }
+        }
+        #endregion
     }
 }
