eadd60097608cdc5891ead585db42c9898583e3b
Timothy Makkison <timmakkison@gmail.com>
2020-09-12 19:55:13 +0100

    Add: ColorToBrush and reversible ColorToHexConverter

---
+++ MVVM Color Utilities/Converters/ColorToHexConverter.cs
@@ -0,0 +1,38 @@
+﻿using MVVM_Color_Utilities.Helpers.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MVVM_Color_Utilities.Converters
+{
+    [ValueConversion(typeof(Color?), typeof(string))]
+    public class ColorToHexConverter : IValueConverter
+    {
+        private readonly Regex hexColorReg = new Regex("^#(?:(?:[0-9a-fA-F]{3}){1,2}|(?:[0-9a-fA-F]{4}){1,2})$");
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = value as Color? ?? Color.White;
+            return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null || !(value is string))
+                return null;
+
+            var s = value as string;
+            if (!hexColorReg.IsMatch(s))
+                return null;
+
+            var c = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(s);
+            return c.ToDrawingColor();
+        }
+    }
+}
