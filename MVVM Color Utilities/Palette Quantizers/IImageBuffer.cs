3e372d7c0be4c3bc58660d7905b0b13c7af50db1
Timothy Makkison <timmakkison@gmail.com>
2021-03-18 14:07:32 +0000

    Create IImageBuffer interface

---
+++ MVVM Color Utilities/Palette Quantizers/IImageBuffer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace MVVM_Color_Utilities.Palette_Quantizers
+{
+    internal interface IImageBuffer
+    {
+        BaseColorQuantizer ActiveQuantizer { get; set; }
+        ConcurrentDictionary<int, int> BitmapColors { get; set; }
+        int ColorCount { get; set; }
+        Bitmap GeneratedBitmap { get; set; }
+        Bitmap OriginalBitmap { get; set; }
+        List<Color> Palette { get; set; }
+
+        Bitmap GenerateNewBitmap();
+        ConcurrentDictionary<int, int> GetBitmapColors();
+        List<Color> GetPalette();
+    }
+}
