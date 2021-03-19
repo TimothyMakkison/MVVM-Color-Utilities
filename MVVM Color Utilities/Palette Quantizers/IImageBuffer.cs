54134afe41967f7ad8ab23777a8a70f8661cf063
Timothy Makkison <timmakkison@gmail.com>
2021-03-19 01:00:57 +0000

    Add additional methods

--- MVVM Color Utilities/Palette Quantizers/IImageBuffer.cs
+++ MVVM Color Utilities/Palette Quantizers/IImageBuffer.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
 
 namespace MVVM_Color_Utilities.Palette_Quantizers
 {
     internal interface IImageBuffer
     {
-        Bitmap GeneratedBitmap { get; set; }
         Bitmap GenerateNewBitmap();
+
         IEnumerable<Color> GetPalette();
+
+        ConcurrentDictionary<int, int> ScanBitmap();
+
+        void SetBitmap(Bitmap bitmap);
+
+        void SetColorCount(int colorCount);
+
+        void SetQuantizer(IColorQuantizer quantizer);
     }
 }
