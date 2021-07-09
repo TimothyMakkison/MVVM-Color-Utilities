3e28b09bd5cc415ba13cb56f29d12ac814a4a51c
Timothy Makkison <timmakkison@gmail.com>
2021-07-10 00:16:45 +0100

    Implement Prism.Unity startup

--- MVVM Color Utilities/App.xaml.cs
+++ MVVM Color Utilities/App.xaml.cs
@@ -1,11 +1,23 @@
 ﻿using System.Windows;
+using MVVM_Color_Utilities.ViewModel;
+using Prism.Ioc;
+using Prism.Unity;
 
 namespace MVVM_Color_Utilities
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : System.Windows.Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainWindow, MainWindowViewModel>();
+        }
     }
 }
