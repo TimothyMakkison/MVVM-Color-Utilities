57fd2555f013af6c153a3e1d5127c1a00db31b81
Timothy Makkison <timmakkison@gmail.com>
2021-07-12 00:45:39 +0100

    Move ServiceCollectionExtensions into into Infrastructure

---
+++ MVVM Color Utilities/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Application.Palette_Quantizers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddColorQuantizers(this IServiceCollection services,
+        params Type[] assemblies)
+    {
+        return services.Scan(scan => scan.FromAssembliesOf(assemblies)
+        .AddClasses(classes => classes.AssignableTo<IColorQuantizer>())
+        .AsImplementedInterfaces()
+        .WithTransientLifetime());
+    }
+
+    public static IServiceCollection AddViewModels(this IServiceCollection services,
+    params Type[] assemblies)
+    {
+        return services.Scan(scan => scan.FromAssembliesOf(assemblies)
+        .AddClasses(classes => classes.Where(t => t.Name.Contains("ViewModel")))
+        .AsImplementedInterfaces()
+        .WithSingletonLifetime());
+    }
+}
