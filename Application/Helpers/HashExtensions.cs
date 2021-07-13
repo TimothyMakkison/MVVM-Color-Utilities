78128fec4bffd0444d24d9fefaabdfa3a15ec63a
Timothy Makkison <timmakkison@gmail.com>
2021-07-13 11:48:09 +0100

    Add GetSequenceHash

---
+++ Application/Helpers/HashExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Application.Helpers
+{
+    public static class HashExtensions
+    {
+        public static int GetSequenceHash<T>(this IEnumerable<T> source)
+        {
+            unchecked
+            {
+                int hash = 19;
+                foreach (var item in source)
+                {
+                    hash += item.GetHashCode();
+                }
+                return hash;
+            }
+        }
+    }
+}
